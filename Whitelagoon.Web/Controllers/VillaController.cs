3a6b3142496b71f53bd2b12327d3d98b4535dafd
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-08-28 19:36:10 +0530

    villa controller View index

---
+++ Whitelagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace Whitelagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Villa> villas=_db.villas.ToList();
+            return View();
+        }
+    }
+}
