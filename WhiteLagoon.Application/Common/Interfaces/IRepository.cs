5b1df88dc52a0a8b359ea34c814b31c0255f7324
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-09-06 05:05:55 +0530

    Repositarty dev wwith interface

---
+++ WhiteLagoon.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includedProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includedProperties = null);
+
+        void Add(T entity);
+        void Remove(T entity);
+
+
+    }
+}
---
+++ WhiteLagoon.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+
+        internal DbSet<T> dbSet;  
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+           dbSet=_db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includedProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);  // Now this accepts the correct filter type
+            }
+
+            if (!string.IsNullOrEmpty(includedProperties))
+            {
+                //Villa,VillaNumber --Case Sensitive
+                foreach (var includeProp in includedProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+
+                }
+            }
+
+            return query.FirstOrDefault();
+
+        }
+
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includedProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includedProperties))
+            {
+                //Villa,VillaNumber --Case Sensitive
+                foreach (var includeProp in includedProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+
+                }
+            }
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+    }
+}
